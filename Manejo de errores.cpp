b0a8728325c7fc5c34976b9a5723c98910ad45b2
Ricardo108 <75130733+Ricardo108@users.noreply.github.com>
2024-01-29 10:18:48 -0600

    Update Manejo de errores.cpp

--- Manejo de errores.cpp
+++ Manejo de errores.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
 int a, b, c;
 
-cout << "dame un numero", a;
-cout <<"dame otro numero", b;
+cout << "dame un numero">> a;
+cout <<"dame otro numero">> b;
 
 c$=a+b;
-cin >> "el resultado es: " c;
+cin >> "el resultado es: ">> c;
 
 }
