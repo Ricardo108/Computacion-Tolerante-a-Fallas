9bfdcc297b6d8133c738c5aa58060dabaeaa8cfc
Ricardo108 <75130733+Ricardo108@users.noreply.github.com>
2024-02-13 13:01:55 -0600

    ahorcado con hilos.py

--- Hilos/ahorcado con hilos.py
+++ Hilos/ahorcado con hilos.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import pickle
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 class AhorcadoGUI:
     def __init__(self, master):
@@ -37,22 +38,21 @@ class AhorcadoGUI:
 
         self.intentos_maximos = 6
 
-        self.partida_terminada = False  # Variable para rastrear si la partida ha terminado
+        self.partida_terminada = False  
 
         self.cargar_partida()
 
-        # Hilo para guardar automáticamente la partida
-        self.thread_guardar_partida = threading.Thread(target=self.guardar_partida_automaticamente)
-        self.thread_guardar_partida.daemon = True
-        self.thread_guardar_partida.start()
+        
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        self.executor.submit(self.guardar_partida_automaticamente)
 
     def iniciar_nueva_partida(self):
-        self.palabra_a_adivinar.set("")  # Borra la palabra a adivinar
-        self.palabra_ingresada.config(state=tk.NORMAL)  # Activa el cuadro de texto
-        self.iniciar_partida_real_btn.config(state=tk.NORMAL)  # Activa el botón "Iniciar"
-        self.adivinar_btn.config(state=tk.DISABLED)  # Desactiva el botón "Adivinar Letra"
-        self.error_label.config(text="")  # Limpia mensajes de error
-        self.partida_terminada = False  # Reinicia la variable de control
+        self.palabra_a_adivinar.set("")  
+        self.palabra_ingresada.config(state=tk.NORMAL) 
+        self.iniciar_partida_real_btn.config(state=tk.NORMAL) 
+        self.adivinar_btn.config(state=tk.DISABLED)  
+        self.error_label.config(text="")  
+        self.partida_terminada = False  
 
     def iniciar_partida(self):
         palabra = self.palabra_a_adivinar.get().lower()
@@ -63,17 +63,17 @@ class AhorcadoGUI:
             self.mostrar_tablero()
             self.guardar_estado()
 
-            self.palabra_ingresada.config(state=tk.DISABLED)  # Desactiva el cuadro de texto
-            self.iniciar_partida_real_btn.config(state=tk.DISABLED)  # Desactiva el botón "Iniciar"
-            self.adivinar_btn.config(state=tk.NORMAL)  # Activa el botón "Adivinar Letra"
-            self.palabra_a_adivinar.set("*" * len(self.palabra))  # Oculta la palabra
-            self.partida_terminada = False  # Reinicia la variable de control
+            self.palabra_ingresada.config(state=tk.DISABLED)
+            self.iniciar_partida_real_btn.config(state=tk.DISABLED)  
+            self.adivinar_btn.config(state=tk.NORMAL)  
+            self.palabra_a_adivinar.set("*" * len(self.palabra))  
+            self.partida_terminada = False  
 
         else:
             self.error_label.config(text="Ingresa una palabra válida.")
 
     def adivinar_letra(self):
-        if not self.partida_terminada:  # Verifica si la partida aún no ha terminado
+        if not self.partida_terminada:  
             letra = self.letra_ingresada.get().lower()
             if letra.isalpha() and len(letra) == 1:
                 if letra in self.letras_adivinadas:
@@ -94,14 +94,14 @@ class AhorcadoGUI:
                     self.iniciar_partida_real_btn.config(state=tk.NORMAL)
                     self.palabra_ingresada.config(state=tk.NORMAL)
                     self.borrar_estado_guardado()
-                    self.partida_terminada = True  # Marca la partida como terminada
+                    self.partida_terminada = True  
 
                 elif self.intentos == self.intentos_maximos:
                     self.error_label.config(text=f"¡Perdiste! La palabra era: {self.palabra}")
                     self.iniciar_partida_real_btn.config(state=tk.NORMAL)
                     self.palabra_ingresada.config(state=tk.NORMAL)
                     self.borrar_estado_guardado()
-                    self.partida_terminada = True  # Marca la partida como terminada
+                    self.partida_terminada = True  
 
             else:
                 self.error_label.config(text="Ingresa una letra válida.")
@@ -170,14 +170,14 @@ class AhorcadoGUI:
             self.mostrar_tablero()
             self.iniciar_partida_real_btn.config(state=tk.DISABLED)
             self.palabra_ingresada.config(state=tk.DISABLED)
-            self.adivinar_btn.config(state=tk.NORMAL)  # Activa el botón "Adivinar Letra"
+            self.adivinar_btn.config(state=tk.NORMAL)
 
     def borrar_estado_guardado(self):
         try:
             with open("ahorcado_estado.pkl", "wb") as archivo_estado:
                 pass
-            self.palabra_ingresada.config(state=tk.NORMAL)  # Activa el cuadro de texto
-            self.adivinar_btn.config(state=tk.DISABLED)  # Desactiva el botón "Adivinar Letra"
+            self.palabra_ingresada.config(state=tk.NORMAL) 
+            self.adivinar_btn.config(state=tk.DISABLED) 
         except FileNotFoundError:
             pass
 
